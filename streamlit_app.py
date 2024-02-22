7d5195ab877024d3b7dab0a1502b12a7a3ddea0c
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-02-21 21:31:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ def draw_water_tank_and_hospital(fill_level):
 
     # Draw pipelines
     ax.plot([tank_width, 10], [0.5 * tank_height, 0.5 * tank_height], 'k-', lw=2)  # Main pipeline
-    ax.plot([10, 10], [hospital_height, hospital_height - 1], 'k-', lw=2)  # Branch pipeline
+    ax.plot([10, 10], [hospital_height, hospital_height - 7], 'k-', lw=2)  # Branch pipeline
     ax.plot([10, 10 + hospital_width], [hospital_height - 1, hospital_height - 1], 'k-', lw=2)  # Connection to hospital
 
     # Customize plot
